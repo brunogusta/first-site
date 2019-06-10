667b780f49261f56f8d24926b6da31bfadeec50e
Bruno Gustavo <brugucolosil95@gmail.com>
2019-06-10 20:47:17 -0300

    css implements

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,4 +2,11 @@ function myFunction() {
   document.getElementById('myDropdown').classList.toggle('show');
 }
 
-//const removeLog = document.querySelector('#eventClick');
+const removeLog = document.querySelector('#eventClick');
+
+window.onclick = function(e) {
+  const contains = removeLog.contains(e.target);
+  if (contains == false) {
+    document.getElementById('myDropdown').classList.remove('show');
+  }
+};
